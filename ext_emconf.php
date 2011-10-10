3e9e498b3fefa5688518384f4a63e7de3ec9f54b
Thomas Löffler <typo3@tomalo.de>
2011-10-10 16:45:48 +0000

    Tagged as version 2.1.1
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/be_secure_pw/tags/2.1.1@52870 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "be_secure_pw".
 #
-# Auto generated 12-01-2011 21:38
+# Auto generated 10-10-2011 17:28
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'You can set password conventions to force secure passwords for BE users. Works with rsa auth and salted passwords!',
 	'category' => 'be',
 	'shy' => 0,
-	'version' => '2.0.0',
+	'version' => '2.1.1',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -41,7 +41,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:20:{s:9:"ChangeLog";s:4:"4594";s:10:"README.txt";s:4:"ee2d";s:21:"ext_conf_template.txt";s:4:"a36c";s:12:"ext_icon.gif";s:4:"70ec";s:17:"ext_localconf.php";s:4:"5606";s:17:"ext_locallang.xml";s:4:"5eb0";s:14:"ext_tables.php";s:4:"2851";s:14:"doc/manual.sxw";s:4:"03cb";s:19:"doc/wizard_form.dat";s:4:"d310";s:20:"doc/wizard_form.html";s:4:"39e1";s:34:"hook/class.user_usersetup_hook.php";s:4:"4489";s:34:"lib/class.tx_besecurepw_secure.php";s:4:"d6e8";s:18:"res/img/accept.png";s:4:"8bfe";s:18:"res/img/cancel.png";s:4:"757a";s:24:"res/js/passwordtester.js";s:4:"ebfa";s:22:"res/lang/locallang.xml";s:4:"00d6";s:33:"res/lang/ux_locallang_csh_mod.xml";s:4:"c02a";s:41:"v4.2/class.ux_SC_mod_user_setup_index.php";s:4:"ab84";s:41:"v4.3/class.ux_SC_mod_user_setup_index.php";s:4:"b7d5";s:41:"v4.4/class.ux_SC_mod_user_setup_index.php";s:4:"c1f6";}',
+	'_md5_values_when_last_written' => 'a:20:{s:9:"ChangeLog";s:4:"4594";s:10:"README.txt";s:4:"ee2d";s:21:"ext_conf_template.txt";s:4:"dea1";s:12:"ext_icon.gif";s:4:"70ec";s:17:"ext_localconf.php";s:4:"5606";s:17:"ext_locallang.xml";s:4:"5092";s:14:"ext_tables.php";s:4:"2851";s:14:"doc/manual.sxw";s:4:"03cb";s:19:"doc/wizard_form.dat";s:4:"d310";s:20:"doc/wizard_form.html";s:4:"39e1";s:34:"hook/class.user_usersetup_hook.php";s:4:"bb64";s:34:"lib/class.tx_besecurepw_secure.php";s:4:"6255";s:18:"res/img/accept.png";s:4:"8bfe";s:18:"res/img/cancel.png";s:4:"757a";s:24:"res/js/passwordtester.js";s:4:"ebfa";s:22:"res/lang/locallang.xml";s:4:"30ba";s:33:"res/lang/ux_locallang_csh_mod.xml";s:4:"c02a";s:41:"v4.2/class.ux_SC_mod_user_setup_index.php";s:4:"35d5";s:41:"v4.3/class.ux_SC_mod_user_setup_index.php";s:4:"b7d5";s:41:"v4.4/class.ux_SC_mod_user_setup_index.php";s:4:"c1f6";}',
 	'suggests' => array(
 	),
 );
