e181a3df2228942d73bad2b7889f83e90f4ce484
Thomas Löffler <loeffler@spooner-web.de>
2015-06-17 21:54:00 +0200

    [TASK] Changes minimum TYPO3 version to 6.2
    Change-Id: I93607fe7407313fce766882ce481e55e7060f39b
    Reviewed-on: http://review.typo3.org/40392
    Reviewed-by: Thomas Löffler <loeffler@spooner-web.de>
    Tested-by: Thomas Löffler <loeffler@spooner-web.de>

--- ext_emconf.php
+++ ext_emconf.php
@@ -25,7 +25,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.0.0-7.99.99',
+			'typo3' => '6.2.0-7.99.99',
 		),
 		'conflicts' => array(
 		),
