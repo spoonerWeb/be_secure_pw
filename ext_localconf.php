f195bf412bea6b7f71454f3de201a389198f9efb
Thomas Löffler <loeffler@spooner-web.de>
2016-04-28 22:20:53 +0200

    [TASK] Some CGL tasks and use GLOBALS
    Change-Id: If519ffbb61b2d0ee106d7b7ffc3321178ff32a09

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,24 +1,29 @@
 <?php
 if (!defined('TYPO3_MODE')) {
-    die ('Access denied.');
+    die('Access denied.');
 }
 
 // here we register "PasswordEvaluator"
 // for editing by tca form
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tce']['formevals']['SpoonerWeb\\BeSecurePw\\Evaluation\\PasswordEvaluator'] = 'EXT:be_secure_pw/Classes/Evaluation/PasswordEvaluator.php';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tce']['formevals']['SpoonerWeb\\BeSecurePw\\Evaluation\\PasswordEvaluator'] =
+    'EXT:be_secure_pw/Classes/Evaluation/PasswordEvaluator.php';
 
 // for editing per "user settings"
 $version7 = \TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger('7.0.0');
 $currentVersion = \TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version);
 if ($currentVersion < $version7) {
-    $TYPO3_CONF_VARS['SC_OPTIONS']['typo3/template.php']['preStartPageHook'][] = 'SpoonerWeb\\BeSecurePw\\Hook\\UserSetupHook->preStartPageHook';
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typo3/template.php']['preStartPageHook']['be_secure_pw'] =
+        'SpoonerWeb\\BeSecurePw\\Hook\\UserSetupHook->preStartPageHook';
 } else {
-    $TYPO3_CONF_VARS['SC_OPTIONS']['ext/setup/mod/index.php']['setupScriptHook'][] = 'SpoonerWeb\\BeSecurePw\\Hook\\UserSetupHook->setupScriptHook';
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/setup/mod/index.php']['setupScriptHook']['be_secure_pw'] =
+        'SpoonerWeb\\BeSecurePw\\Hook\\UserSetupHook->setupScriptHook';
 }
-$TYPO3_CONF_VARS['SC_OPTIONS']['typo3/template.php']['moduleBodyPostProcess'][] = 'SpoonerWeb\\BeSecurePw\\Hook\\UserSetupHook->moduleBodyPostProcess';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typo3/template.php']['moduleBodyPostProcess']['be_secure_pw'] =
+    'SpoonerWeb\\BeSecurePw\\Hook\\UserSetupHook->moduleBodyPostProcess';
 
 // password reminder
-$TYPO3_CONF_VARS['SC_OPTIONS']['typo3/backend.php']['constructPostProcess'][] = 'SpoonerWeb\\BeSecurePw\\Hook\\BackendHook->constructPostProcess';
-$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['be_secure_pw'] = 'SpoonerWeb\\BeSecurePw\\Hook\\BackendHook';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typo3/backend.php']['constructPostProcess']['be_secure_pw'] =
+    'SpoonerWeb\\BeSecurePw\\Hook\\BackendHook->constructPostProcess';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['be_secure_pw'] =
+    'SpoonerWeb\\BeSecurePw\\Hook\\BackendHook';
 
-?>
