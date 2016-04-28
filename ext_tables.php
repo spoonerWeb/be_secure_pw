987cae04c3976b04caa32813a894a7e6dc735beb
Thomas Löffler <loeffler@spooner-web.de>
2016-04-28 23:57:20 +0200

    [REVERT] Switches back the code from ext_localconf to ext_tables
    Change-Id: I6b9fcb8e4f1e85f2d7e7219e46ef675b55104f12

--- ext_tables.php
+++ ext_tables.php
@@ -3,5 +3,14 @@ if (!defined('TYPO3_MODE')) {
     die ('Access denied.');
 }
 
+/* set password evaluation for password field in be_users */
+$evaluation = [
+    'required',
+    \SpoonerWeb\BeSecurePw\Evaluation\PasswordEvaluator::class,
+    \TYPO3\CMS\Saltedpasswords\Evaluation\BackendEvaluator::class,
+    'password'
+];
+$GLOBALS['TCA']['be_users']['columns']['password']['config']['eval'] = implode(',', $evaluation);
+
 /* override language file */
 $TCA_DESCR['_MOD_user_setup']['refs'][] = 'EXT:be_secure_pw/Resources/Private/Language/ux_locallang_csh_mod.xml';
