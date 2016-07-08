c177c3891b56d3372099167f4e1726144524a398
Thomas Löffler <loeffler@spooner-web.de>
2016-07-08 16:56:13 +0200

    [BUGFIX] Uses right salted password instance in 6.2
    Change-Id: I4e13d1dfafcb3da5f03d74d1d83c437f6e58bab6

--- ext_tables.php
+++ ext_tables.php
@@ -3,11 +3,21 @@ if (!defined('TYPO3_MODE')) {
     die ('Access denied.');
 }
 
+// for editing per "user settings"
+$version7 = \TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger('7.0.0');
+$currentVersion = \TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version);
+
+if ($currentVersion < $version7) {
+    $saltedPasswordEvaluator = 'tx_saltedpasswords_eval_be';
+} else {
+    $saltedPasswordEvaluator = \TYPO3\CMS\Saltedpasswords\Evaluation\BackendEvaluator::class;
+}
+
 /* set password evaluation for password field in be_users */
 $evaluation = [
     'required',
     \SpoonerWeb\BeSecurePw\Evaluation\PasswordEvaluator::class,
-    \TYPO3\CMS\Saltedpasswords\Evaluation\BackendEvaluator::class,
+    $saltedPasswordEvaluator,
     'password'
 ];
 $GLOBALS['TCA']['be_users']['columns']['password']['config']['eval'] = implode(',', $evaluation);
