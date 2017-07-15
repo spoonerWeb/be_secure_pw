1e36ea2b8223d8d2741ce350d01edc40ff76c0d1
Thomas Löffler <loeffler@spooner-web.de>
2017-07-15 10:12:26 +0200

    [BUGFIX] Fixes coding style in ext_* files
    Change-Id: Id3bff748fd440d933a219a26aaf3db3457f8addc

--- ext_tables.php
+++ ext_tables.php
@@ -1,7 +1,6 @@
 <?php
-if (!defined('TYPO3_MODE')) {
-    die ('Access denied.');
-}
+
+defined('TYPO3_MODE') || die('Access denied.');
 
 // for editing per "user settings"
 $version7 = \TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger('7.0.0');
