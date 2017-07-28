41ec98b126cb5f447d140fbe7bb7a6578b65e624
Thomas Löffler <loeffler@spooner-web.de>
2017-07-28 09:26:11 +0200

    [TASK] Removes 6.2 code and uses $boot function
    Change-Id: I1046a5320d22ba732e90b27252a839da6722fa4f

--- ext_tables.php
+++ ext_tables.php
@@ -1,25 +1,23 @@
 <?php
-
 defined('TYPO3_MODE') || die('Access denied.');
 
-// for editing per "user settings"
-$version7 = \TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger('7.0.0');
-$currentVersion = \TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version);
-
-if ($currentVersion < $version7) {
-    $saltedPasswordEvaluator = 'tx_saltedpasswords_eval_be';
-} else {
+$boot = function() {
+    // for editing per "user settings"
     $saltedPasswordEvaluator = \TYPO3\CMS\Saltedpasswords\Evaluation\BackendEvaluator::class;
-}
 
-/* set password evaluation for password field in be_users */
-$evaluation = [
-    'required',
-    \SpoonerWeb\BeSecurePw\Evaluation\PasswordEvaluator::class,
-    $saltedPasswordEvaluator,
-    'password'
-];
-$GLOBALS['TCA']['be_users']['columns']['password']['config']['eval'] = implode(',', $evaluation);
+    // set password evaluation for password field in be_users
+    $evaluation = [
+        'required',
+        \SpoonerWeb\BeSecurePw\Evaluation\PasswordEvaluator::class,
+        $saltedPasswordEvaluator,
+        'password'
+    ];
+    $GLOBALS['TCA']['be_users']['columns']['password']['config']['eval'] = implode(',', $evaluation);
 
-/* override language file */
-$TCA_DESCR['_MOD_user_setup']['refs'][] = 'EXT:be_secure_pw/Resources/Private/Language/ux_locallang_csh_mod.xml';
+    /* override language file */
+    $GLOBALS['TCA_DESCR']['_MOD_user_setup']['refs'][] =
+        'EXT:be_secure_pw/Resources/Private/Language/ux_locallang_csh_mod.xml';
+};
+
+$boot();
+unset($boot);
