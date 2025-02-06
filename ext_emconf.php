bf419debfefda0e6c26a1935236cc1194fd90a91
Thomas Löffler <loeffler@spooner-web.de>
2025-02-06 13:42:27 +0100

    [TASK] Add PHP 8.3 support in ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -19,7 +19,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '11.5.0-11.5.99',
-            'php' => '7.4.0-8.2.99',
+            'php' => '7.4.0-8.3.99',
         ],
         'conflicts' => [],
         'suggests' => [],
